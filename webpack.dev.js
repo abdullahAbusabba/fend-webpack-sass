46f00db3a2f00a3d286bfdd28fa8d96f78ba5091
abdullahAbusabba <alajmi7abdullah@gmail.com>
2020-11-22 06:05:57 +0300

    add js rule and import scss in index.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,8 +10,13 @@ module.exports = {
     stats: 'verbose',
     module: {
         rules: [
+        {
+            test:/\.js$/,
+            exclude: /node_modules/,
+            loader:"babel-loader"
+        },
             {
-                test: '/\.scss$/',
+                test: /\.scss$/,
                 exclude: /node_modules/,
                 loader: ['style-loader', 'css-loader', 'sass-loader']
             }
