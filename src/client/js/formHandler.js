36e8afadff881d076990202523760fc57b666568
abdullahAbusabba <alajmi7abdullah@gmail.com>
2020-11-22 08:01:31 +0300

    add fetch request to weather api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,22 @@
+
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
+    let url = 'http://api.openweathermap.org/data/2.5/weather?zip=12222&appid=94599f14bd184627cda123469c9d525d&units=metric';
 
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(url)
     .then(res => {
         return res.json()
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        document.getElementById('results').innerHTML = "temperature:"+data.main.temp+"<br> wather description:"+data.weather[0].description;
+        
     })
 }
 
