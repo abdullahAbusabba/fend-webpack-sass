c223ab9fc9357b3d7d9b5918abe8a318b890b568
abdullahAbusabba <alajmi7abdullah@gmail.com>
2020-11-23 06:18:48 +0300

    update api key

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,7 +5,7 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    let url = 'http://api.openweathermap.org/data/2.5/weather?zip=12222&appid=94599f14bd184627cda123469c9d525d&units=metric';
+    let url = '';
 
     Client.checkForName(formText)
 
